1ab4d889691c5dac20d723a318e59cf267f7e421
Shahbaz Ahmed <onlyshahbaz@live.com>
2024-09-17 19:25:19 +0500

    First Commit

---
+++ add_two_numbers.py
@@ -0,0 +1,13 @@
+num1 = int (input("Enter first number: "))
+num2 = int (input("Enter second number: "))
+sum = num1 + num2
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+#This line prints a formatted string:
+
+'''
+{0}, {1}, and {2} are placeholders
+The .format() method replaces these placeholders with the values of num1, num2, and sum respectively
+The result is a sentence stating the two numbers and their sum
+
+'''
